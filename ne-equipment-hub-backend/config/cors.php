a739f42c5741afb73745b07387e1306780fbe136
CelsoSebastiao <142792582+CelsoSebastiao@users.noreply.github.com>
2026-03-26 21:28:43 +0200

    Update cors.php
    comit

--- ne-equipment-hub-backend/config/cors.php
+++ ne-equipment-hub-backend/config/cors.php
@@ -3,7 +3,12 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', '*')],
+    'allowed_origins' => [
+        'https://ne-equipment.vercel.app',
+        'http://localhost:5173',
+        'http://localhost:3000',
+        env('FRONTEND_URL'),
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
